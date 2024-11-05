115db929e334010e904e7101a7a2fbea24ac5243
cmgvc <chloe.gavv@gmail.com>
2024-11-05 02:11:06 -0500

    add navbar animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import '../styles/Navbar.css'; 
+// src/components/Navbar.js
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap'; // Import GSAP for animations
+import '../styles/Navbar.css';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
+    const navbarRef = useRef(null);
+
+    useEffect(() => {
+        // Animate the navbar when it mounts
+        gsap.fromTo(navbarRef.current, 
+            { y: -100, opacity: 0 }, // Start off-screen and transparent
+            { y: 0, opacity: 1, duration: 1, ease: 'power4.out' } // Animate to original position and full opacity
+        );
+    }, []);
+
     return (
-        <nav id="navbar">
+        <nav ref={navbarRef} id="navbar">
             <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
+                <li><Link to="about" smooth={true} duration={500}>About</Link></li>
+                <li><Link to="services" smooth={true} duration={500}>Services</Link></li>
+                <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
             </ul>
         </nav>
     );
-};
+}
 
 export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
-// src/pages/Home.js
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import '../App.css';
 import '../styles/Home.css';
 import { handleAnimateLastName, handleAnimateFirstName, handleFinalAnimation } from '../animations/handleAnimateHome';
@@ -8,12 +7,15 @@ import Navbar from '../components/Navbar';
 function Home() {
     const lastNameRef = useRef(null);
     const firstNameRef = useRef(null);
+    const [isNavbarVisible, setIsNavbarVisible] = useState(false);
 
     useEffect(() => {
         if (lastNameRef.current) {
             handleAnimateLastName(lastNameRef.current).then(() => {
                 handleAnimateFirstName(firstNameRef.current).then(() => {
-                    handleFinalAnimation(firstNameRef.current, lastNameRef.current);
+                    handleFinalAnimation(firstNameRef.current, lastNameRef.current).then(() => {
+                        setIsNavbarVisible(true);
+                    });
                 });
             });
         }
@@ -30,6 +32,7 @@ function Home() {
                         Chloe
                     </div>
                 </div>
+                {isNavbarVisible && <Navbar />} 
             </div>
         </div>
     );
