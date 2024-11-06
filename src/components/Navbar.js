ec22c611a68c48c2e71f31ee357f8b539da42503
cmgvc <chloe.gavv@gmail.com>
2024-11-06 14:50:18 -0500

    fix gsap import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { gsap } from 'gsap'; 
+import { gsap } from "gsap";
 import '../styles/Navbar.css';
 import { Link } from 'react-scroll';
 
