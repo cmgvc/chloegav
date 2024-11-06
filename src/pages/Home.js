6cc149a6ad245f6cdc140d85281c5cb12c40bb48
cmgvc <chloe.gavv@gmail.com>
2024-11-06 02:37:52 -0500

    add nametag animation to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,11 @@ import '../styles/Home.css';
 import { handleAnimateLastName, handleAnimateFirstName, handleFinalAnimation } from '../animations/handleAnimateHome';
 import Navbar from '../components/Navbar';
 import Type from '../components/Type.js';
+import laptopImg from "../assets/headshot.png";
 
 function Home() {
     const lastNameRef = useRef(null);
-    const firstNameRef = useRef(null);
+    const firstNameRef = useRef(null); 
     const [isHomeLoaded, setIsHomeLoaded] = useState(false);
 
     useEffect(() => {
@@ -33,17 +34,41 @@ function Home() {
                         Chloe
                     </div>
                 </div>
-                {isHomeLoaded ? (
-                    <div>
-                        <div className="typewriter">
-                            <Type />
-                        </div>
-                        <Navbar />
-                    </div>)
-                    
-                
-                : null} 
             </div>
+            {isHomeLoaded ? (
+                <div>
+                    <Navbar />
+                    <div className="typewriter">
+                        <Type />
+                    </div>
+                    <div className='home-container'>
+                    <div className="name-tag-container">
+                        <div className="name-tag-card">
+                            <div className="name-tag-front">
+                                <div className="name-tag-header">Hi my name is</div>
+                                <p>CHLOE</p>
+                            </div>
+
+                            <div className="name-tag-back">
+                                <img src={laptopImg}></img>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div className={`about-me`}>
+                        <h1>/about</h1>
+                        <p>
+                            I am a full stack developer with a passion for creating beautiful and functional web applications. I have experience with HTML, CSS, JavaScript, React, Node.js, Express, MongoDB, and more. I am a quick learner and am always looking to expand my skill set. I am currently looking for a full-time position as a software engineer.
+                        </p>   
+                    </div>
+                    <div className={`experience`}>
+                        <h1>/experience</h1>
+                        <p>
+                            I have experience with HTML, CSS, JavaScript, React, Node.js, Express, MongoDB, and more. I have created several web applications, including a weather app, a chat app, and a portfolio website. I am a quick learner and am always looking to expand my skill set. I am currently looking for a full-time position as a software engineer.
+                        </p>
+                        </div>
+                </div>
+            ) : null}
         </div>
     );
 }
